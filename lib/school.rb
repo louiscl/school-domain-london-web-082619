14f68d38a7e3f341e436a03d19de5614546117bb
github username <github email address>
2019-08-28 23:02:32 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,27 +1,34 @@
 require "pry"
 
 class School
-  
-    ROSTER = {}
     
     def initialize(name)
       @name = name
+      @roster = {}
     end
     
     def roster 
-      ROSTER
+      @roster
     end
     
     def add_student(new_student, grade)
-        ROSTER.any? do |k, v|
-            binding.pry
-        if k == grade
-            HASH[grade] << new_student
-        else
-        ROSTER[grade] = []
-        ROSTER[grade] << new_student
+        @student_name = new_student
+        @grade = grade
+      if @roster.include?(grade) == false
+          @roster[grade] = []
         end
+        @roster[grade] << new_student
     end
+
+    def grade(num)
+      @roster[num]
+    end
+
+    def sort
+      @roster.each do |grade, student|
+        @roster[grade] = student.sort
+    end
+
 end
 
 
