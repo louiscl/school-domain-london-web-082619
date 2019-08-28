3d20f80f401e9c288d29ad21ee2c33855d92d57b
github username <github email address>
2019-08-28 22:05:35 +0100

    school attempt

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require "pry"
+
+class School
+  
+    ROSTER = {}
+    
+    def initialize(name)
+      @name = name
+    end
+    
+    def roster 
+      ROSTER
+    end
+    
+    def add_student(new_student, grade)
+        ROSTER.any? do |k, v|
+            binding.pry
+        if k == grade
+            HASH[grade] << new_student
+        else
+        ROSTER[grade] = []
+        ROSTER[grade] << new_student
+        end
+    end
+end
+
+
+
+end
