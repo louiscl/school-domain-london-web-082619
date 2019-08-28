c483a0e2388c149586ac33c6afd00ff6c9b22fbd
Louis Lammerting <louis.lammerting@gmail.com>
2019-08-28 19:32:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+
+class School
+  
+  ROSTER = {}
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster 
+    ROSTER
+  end
+  
+  def add_student(new_student, grade)
+    ROSTER.merge!(grade => new_student)
+  end
+    
+  
+end 
